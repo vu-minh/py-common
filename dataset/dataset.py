0e6b20e83c0ad2b1b311ddb8418d724a646244fe
vu-minh <vuvietminh@unamur.be>
2019-02-19 01:08:02 +0100

    Refactor naming

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,11 +4,12 @@
 import os
 from collections import namedtuple
 from functools import partial
+import pickle
 
 import numpy as np  # type: ignore
-from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler  # type: ignore
+from sklearn.preprocessing import StandardScaler  # type: ignore
+from sklearn.preprocessing import Normalizer, MinMaxScaler  # type: ignore
 import sklearn.datasets as sk_datasets  # type: ignore
-import pickle
 
 from typing import List, Dict, Tuple
 from typing import Callable
@@ -45,23 +46,23 @@ def load_pickle(name: str) -> Dict:
     return data
 
 
-def load_fashion(N: int) -> Callable:
+def fashion_loader(N: int) -> Callable:
     return partial(load_pickle, f"{data_config.DATA_HOME}/fashion/{N}.pkl")
 
 
-def load_quickdraw(N: int) -> Callable:
+def quickdraw_loader(N: int) -> Callable:
     return partial(load_pickle, f"{data_config.DATA_HOME}/quickdraw/{N}.pkl")
 
 
-def get_load_funcs() -> Dict[str, Callable]:
+def get_data_loaders() -> Dict[str, Callable]:
     """Build a mapping from dataset name to the function for loading data.
     Since the function makes use of the path to the data file,
     they must be built on the fly to take the newest values of DATA_HOME
     """
     return dict(
-        [(f"FASHION{N}", load_fashion(N))
+        [(f"FASHION{N}", fashion_loader(N))
          for N in [100, 200, 500]] +
-        [(f"QUICKDRAW{N}", load_quickdraw(N))
+        [(f"QUICKDRAW{N}", quickdraw_loader(N))
          for N in [50, 90, 100, 120, 200, 500, 1000]] +
         [("IRIS", sk_datasets.load_iris),
          ("DIGITS", sk_datasets.load_digits),
@@ -81,12 +82,12 @@ def load_dataset(name: str, preprocessing_method: str = 'standardize',
     Return:
         Tuple of np array: X_original, X_processed, y
     """
-    func_mapping = get_load_funcs()
-    load_func = func_mapping.get(name, None)
+    data_loaders = get_data_loaders()
+    load_func = data_loaders.get(name, None)
     if load_func is None:
         raise ValueError("{} dataset is not available."
                          "The available ones are:\n\t{}".format(
-                            name, '\n\t'.join(func_mapping.keys())))
+                            name, '\n\t'.join(data_loaders.keys())))
 
     data = load_func()
     X_original = data['data'].astype(dtype)
@@ -104,5 +105,5 @@ def load_dataset(name: str, preprocessing_method: str = 'standardize',
 if __name__ == '__main__':
     set_data_home('./data')
     print(get_data_home())
-    X_original, X, y = load_dataset('FASHION100')
+    X_original, X, y = load_dataset('BREAST_CANCER')
     print(X_original.shape, X.shape, y.shape)
