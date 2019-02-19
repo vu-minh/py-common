d4b8229563c2933bd38918b6c211b1ab1208740e
vu-minh <vuvietminh@unamur.be>
2019-02-19 01:32:32 +0100

    Add basic test dataset

--- test_dataset.py
+++ test_dataset.py
@@ -2,9 +2,11 @@
 '''
 
 import unittest
+import numpy as np
 
 from dataset.dataset import set_data_home, get_data_home
 from dataset.dataset import list_datasets
+from dataset.dataset import load_dataset
 
 
 class DatasetTestCase(unittest.TestCase):
@@ -17,6 +19,40 @@ class DatasetTestCase(unittest.TestCase):
     def test_list_dir(self):
         self.assertIsInstance(list_datasets(), list)
 
+    def test_dataset_name_does_not_exist(self):
+        with self.assertRaises(ValueError):
+            load_dataset('NameNotExist')
+
+    def test_dataset_size(self):
+        expected_size = dict(
+            FASHION100=[(100, 28*28), (100, 28*28), (100,)],
+            QUICKDRAW200=[(200, 28*28), (200, 28*28), (200,)],
+            IRIS=[(150, 4), (150, 4), (150,)],
+            DIGITS=[(1797, 8*8), (1797, 8*8), (1797,)],
+            WINE=[(178, 13), (178, 13), (178,)],
+            BREAST_CANCER=[(569, 30), (569, 30), (569,)]
+        )
+
+        for dataset_name, dataset_size in expected_size.items():
+            data = load_dataset(dataset_name)
+            self.assertListEqual(
+                list(map(lambda X: X.shape, data)), dataset_size
+            )
+
+    def test_dataset_type(self):
+        for dataset_name in [
+            'FASHION100', 'QUICKDRAW200',
+            'IRIS', 'DIGITS', 'WINE', 'BREAST_CANCER'
+        ]:
+            data = load_dataset(dataset_name)
+            self.assertListEqual(
+                list(map(lambda X: X.dtype, data)), [np.float32]*3
+            )
+
+# TODO TestCase class to test data preprocessing:
+# test_standardized_data (zero-mean, unit-variance)
+# test_normalized_data (unit-variance)
+# test_unit_scaled_data (min=0, max=1)
 
 
 if __name__ == "__main__":
