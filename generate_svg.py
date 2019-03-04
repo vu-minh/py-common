6c6be728c72929a5e44710267462ccde106fda30
vu-minh <vuvietminh@unamur.be>
2019-03-05 00:51:50 +0100

    Util to pack all images of a dataset into an stacked SVG

---
+++ generate_svg.py
@@ -0,0 +1,30 @@
+import math
+import numpy as np
+from dataset.dataset import set_data_home, load_dataset
+from plot.export_utils import generate_stacked_svg
+
+
+set_data_home('./data')
+output_svg_dir = './data/svg'
+list_dataset_names = [
+    # 'FASHION100', 'FASHION200',
+    # 'QUICKDRAW100', 'QUICKDRAW200',
+    'COIL20_100', 'COIL20_200'
+]
+
+for dataset_name in list_dataset_names:
+    X_original, X, y = load_dataset(dataset_name)
+    print('Dataset: ', dataset_name, X.shape, y.shape)
+    if dataset_name.lower().startswith('coil'):
+        N, D = X.shape
+        img_size = int(math.sqrt(D))
+        X_original = np.swapaxes(X_original.reshape(N, img_size, img_size),
+                                 axis1=1, axis2=2)
+        X_original = X_original.reshape(N, -1)
+
+    for cmap_type in ['gray', 'gray_r', 'color']:
+        labels = y if cmap_type == 'color' else None
+        svg_out_name = f"{output_svg_dir}/{dataset_name}_{cmap_type}.svg"
+        print('Generate: ', svg_out_name)
+        generate_stacked_svg(svg_out_name=svg_out_name, dataset=X_original,
+                             labels=labels, default_cmap=cmap_type)
---
+++ plot/export_utils.py
@@ -0,0 +1,79 @@
+import math
+import base64
+from io import BytesIO
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import to_rgb
+
+
+SVG_META_DATA = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<!-- Created with matplotlib (http://matplotlib.org/),
+    modified to stack multiple svg elemements,
+    used for packing all images in a dataset
+-->
+<svg version="1.1" width="22" height="22" viewBox="0 0 22 22"
+     xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink">
+<defs>
+<style type="text/css">
+    *{stroke-linecap:butt;stroke-linejoin:round;}
+    .sprite { display: none;}
+    .sprite:target { display: inline; }
+    .packed-svg-custom {/*override this css to customize style for svg image*/}
+</style>
+</defs>
+"""
+
+SVG_IMG_TAG = """
+<g class="sprite" id="{}">
+    <image class="packed-svg-custom"
+        id="stacked_svg_img_{}"
+        width="20"
+        height="20"
+        xlink:href="data:image/png;base64,{}"
+    />
+</g>
+"""
+
+
+def generate_stacked_svg(svg_out_name, dataset,
+                         labels=None, default_cmap='gray_r'):
+    """Create an SVG to store all image of a `dataset`.
+        To access an image, use svg_out_name.svg#img_id, e.g. MNIST.svg#123
+    """
+    # current_dpi = plt.gcf().get_dpi()
+    # fig = plt.figure(figsize=(28 / current_dpi, 28 / current_dpi))
+
+    def _create_cm(basecolor):
+        colors = [(1, 1, 1), to_rgb(basecolor), to_rgb(basecolor)]  # R->G->B
+        return LinearSegmentedColormap.from_list(colors=colors, name=basecolor)
+
+    def _create_custom_cmap():
+        basecolors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+                      "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+        return list(map(_create_cm, basecolors))
+
+    def _generate_figure_data(img, cmap):
+        fig_file = BytesIO()
+        plt.imsave(fig_file, img, cmap=cmap)
+        plt.gcf().clear()
+        fig_file.seek(0)
+        return base64.b64encode(fig_file.getvalue()).decode('utf-8')
+
+    N, D = dataset.shape
+    img_size = int(math.sqrt(D))
+    custom_cmap = _create_custom_cmap()
+
+    with open(svg_out_name, 'w') as svg_file:
+        svg_file.write(SVG_META_DATA)
+
+        for i in range(N):
+            img = dataset[i].reshape(img_size, img_size)
+            cmap = (default_cmap if labels is None
+                    else custom_cmap[int(labels[i]) % 10])
+            fig_data = _generate_figure_data(img, cmap)
+            svg_file.write(SVG_IMG_TAG.format(i, i, fig_data))
+
+        svg_file.write("</svg>")
